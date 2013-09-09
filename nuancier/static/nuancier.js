dae40bd4abafa4b9d8a34a4ea4a37d3eafca51ec
Ryan Lerch <ryanlerch@fedoraporject.org>
2013-09-09 09:50:52 -0400

    started messing with the vote page

--- nuancier/static/nuancier.js
+++ nuancier/static/nuancier.js
@@ -3,10 +3,8 @@ var cnter = 0;
 function updateSelectionCnt(cb) {
   if (cb.checked){
     cnter += 1;
-    $(cb).parent().parent().css('background-color', '#3C6EB4');
   } else {
     cnter -= 1;
-    $(cb).parent().parent().css('background-color', '#FFF');
   }
   
   var popup = document.getElementById('fix_info');
