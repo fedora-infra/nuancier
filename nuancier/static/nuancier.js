03fe258c18a9a4c19de1e1360353de83f30810e7
Ryan Lerch <ryanlerch@fedoraporject.org>
2013-09-20 10:18:57 -0400

    added sidebar functionality

--- nuancier/static/nuancier.js
+++ nuancier/static/nuancier.js
@@ -1,13 +1,35 @@
 var cnter = 0;
 
-function updateSelectionCnt(cb) {
-  if (cb.checked){
-    cnter += 1;
-  } else {
-    cnter -= 1;
-  }
-  
-  var popup = document.getElementById('fix_info');
-  popup.innerHTML = cnter + ' pictures selected';
-  popup.className = "large_button";
+function checkboxCheck(checkbox)
+{
+	theid = checkbox.attr('id');
+	checkbox.siblings('label').children('.smallthumb').clone().attr("id", "sidethumb-"+theid).appendTo($("#sideimages"));
+	$("#sidethumb-"+theid).click(function(){
+		myid = $(this).attr("id").replace("sidethumb-","");
+		alert(myid);
+		$("#"+myid).prop('checked', false);
+		$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
+		$("#sidethumb-"+myid).remove();
+		$('input[type=checkbox]').prop('disabled', false);
+	});
 }
+
+$( document ).ready(function() {
+	/*There may be some already checked if the user soft refreshes the page
+	  so this makes sure that they are moved to the sidebar and the count is
+	  correct*/
+	$('input[type=checkbox]:checked').each(function(){checkboxCheck($(this));});
+	$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
+
+
+	$('input[type=checkbox]').change(function() {
+		if ($('input[type=checkbox]:checked').length == 16)
+		{
+		$('input[type=checkbox]').prop('disabled', true)
+		}
+		if($(this).is(':checked')) {
+			checkboxCheck($(this));
+			$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
+		}
+	});
+});
