0d81c0b1844eb407fbf69171afc7b1730dc5429b
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-23 11:03:27 +0200

    Use attr instead of prop and refactor some code in the JS

--- nuancier/static/nuancier.js
+++ nuancier/static/nuancier.js
@@ -3,46 +3,49 @@ var cnter = 0;
 function checkboxCheck(checkbox)
 {
     theid = checkbox.attr('id');
-    $(theid).prop('checked', true);
+    $(theid).attr('checked', true);
     checkbox.siblings('label').children('.smallthumb').clone(
         ).attr("id", "sidethumb-"+theid).appendTo($("#sideimages"));
     $("#sidethumb-"+theid).click(function(){
         myid = $(this).attr("id").replace("sidethumb-","");
-        $("#"+myid).prop('checked', false);
+        $("#"+myid).attr('checked', false);
         $("#fix_info").html($('input[type=checkbox]:checked'
             ).length + " of " + votelimit);
         $("#sidethumb-"+myid).remove();
-        $('input[type=checkbox]').prop('disabled', false);
+        $('input[type=checkbox]').attr('disabled', false);
         $(".hoveroverlay").removeClass("limit");
         $(".resizelink").removeClass("limit");
     });
 }
+
+
+function updateActionAndInfo(){
+    if ($('input[type=checkbox]:checked').length == votelimit) {
+        $('input[type=checkbox]').attr('disabled', true);
+        $(".hoveroverlay").addClass("limit");
+        $(".resizelink").addClass("limit");
+
+        $("#fix_info").html($('input[type=checkbox]:checked'
+        ).length + " of " + votelimit + " -- You have reached the "
+        + "maximum number of votes you can cast.");
+    } else {
+        $("#fix_info").html($('input[type=checkbox]:checked'
+        ).length + " of " + votelimit);
+    }
+}
+
 
 $( document ).ready(function() {
     /*There may be some already checked if the user soft refreshes the page
       so this makes sure that they are moved to the sidebar and the count is
       correct*/
     $('input[type=checkbox]:checked').each(function(){checkboxCheck($(this));});
-    $("#fix_info").html($('input[type=checkbox]:checked'
-        ).length + " of " + votelimit);
-
+    updateActionAndInfo();
 
     $('input[type=checkbox]').change(function() {
-        if ($('input[type=checkbox]:checked').length == votelimit) {
-            $('input[type=checkbox]').prop('disabled', true);
-            $(".hoveroverlay").addClass("limit");
-            $(".resizelink").addClass("limit");
-        }
         if($(this).is(':checked')) {
             checkboxCheck($(this));
-            if ($('input[type=checkbox]:checked').length == votelimit) {
-                $("#fix_info").html($('input[type=checkbox]:checked'
-                ).length + " of " + votelimit + " -- You have reached the "
-                + "maximum number of votes you can cast.");
-            } else {
-                $("#fix_info").html($('input[type=checkbox]:checked'
-                ).length + " of " + votelimit);
-            }
+            updateActionAndInfo();
         }
     });
 });
