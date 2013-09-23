2c747145c89348558e9cf6e8cae6d3a1f1954082
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-23 09:56:47 +0200

    Llittle code formatting changes and warn user when maximum is reached

--- nuancier/static/nuancier.js
+++ nuancier/static/nuancier.js
@@ -2,37 +2,46 @@ var cnter = 0;
 
 function checkboxCheck(checkbox)
 {
-	theid = checkbox.attr('id');
-	checkbox.siblings('label').children('.smallthumb').clone().attr("id", "sidethumb-"+theid).appendTo($("#sideimages"));
-	$("#sidethumb-"+theid).click(function(){
-		myid = $(this).attr("id").replace("sidethumb-","");
-		$("#"+myid).prop('checked', false);
-		$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
-		$("#sidethumb-"+myid).remove();
-		$('input[type=checkbox]').prop('disabled', false);
-		$(".hoveroverlay").removeClass("limit");
-		$(".resizelink").removeClass("limit");
-	});
+    theid = checkbox.attr('id');
+    checkbox.siblings('label').children('.smallthumb').clone(
+        ).attr("id", "sidethumb-"+theid).appendTo($("#sideimages"));
+    $("#sidethumb-"+theid).click(function(){
+        myid = $(this).attr("id").replace("sidethumb-","");
+        $("#"+myid).prop('checked', false);
+        $("#fix_info").html($('input[type=checkbox]:checked'
+            ).length + " of " + votelimit);
+        $("#sidethumb-"+myid).remove();
+        $('input[type=checkbox]').prop('disabled', false);
+        $(".hoveroverlay").removeClass("limit");
+        $(".resizelink").removeClass("limit");
+    });
 }
 
 $( document ).ready(function() {
-	/*There may be some already checked if the user soft refreshes the page
-	  so this makes sure that they are moved to the sidebar and the count is
-	  correct*/
-	$('input[type=checkbox]:checked').each(function(){checkboxCheck($(this));});
-	$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
+    /*There may be some already checked if the user soft refreshes the page
+      so this makes sure that they are moved to the sidebar and the count is
+      correct*/
+    $('input[type=checkbox]:checked').each(function(){checkboxCheck($(this));});
+    $("#fix_info").html($('input[type=checkbox]:checked'
+        ).length + " of " + votelimit);
 
 
-	$('input[type=checkbox]').change(function() {
-		if ($('input[type=checkbox]:checked').length == votelimit)
-		{
-		$('input[type=checkbox]').prop('disabled', true);
-		$(".hoveroverlay").addClass("limit");
-		$(".resizelink").addClass("limit");
-		}
-		if($(this).is(':checked')) {
-			checkboxCheck($(this));
-			$("#fix_info").html($('input[type=checkbox]:checked').length + " of " + votelimit);
-		}
-	});
+    $('input[type=checkbox]').change(function() {
+        if ($('input[type=checkbox]:checked').length == votelimit) {
+            $('input[type=checkbox]').prop('disabled', true);
+            $(".hoveroverlay").addClass("limit");
+            $(".resizelink").addClass("limit");
+        }
+        if($(this).is(':checked')) {
+            checkboxCheck($(this));
+            if ($('input[type=checkbox]:checked').length == votelimit) {
+                $("#fix_info").html($('input[type=checkbox]:checked'
+                ).length + " of " + votelimit + " -- You have reached the "
+                + "maximum number of votes you can cast.");
+            } else {
+                $("#fix_info").html($('input[type=checkbox]:checked'
+                ).length + " of " + votelimit);
+            }
+        }
+    });
 });
