cf0eb5640037952221c2e5f54b364da5fddcb294
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-01 08:13:48 +0200

    Add script to generate the cache of an election by hand

---
+++ generate_cache.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+import os
+
+os.environ['NUANCIER_CONFIG'] = '/etc/nuancier/nuancier-lite.cfg'
+
+from nuancier import APP, SESSION, lib
+
+election = lib.get_election(SESSION, 1)
+lib.generate_cache(
+    SESSION,
+    election,
+    APP.config['PICTURE_FOLDER'],
+    APP.config['CACHE_FOLDER'],
+    APP.config['THUMB_SIZE'],
+)
