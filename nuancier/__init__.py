9b7dcabe32c961f0610c111b341d4e03201c2649
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-01 21:43:58 +0100

    Remove the toggling mechanisms from the controller and admin template

--- nuancier/__init__.py
+++ nuancier/__init__.py
@@ -434,37 +434,6 @@ def admin_index():
     return flask.render_template('admin_index.html', elections=elections)
 
 
-@APP.route('/admin/new/', methods=['GET', 'POST'])
-@nuancier_admin_required
-def admin_new():
-    ''' Create a new election. '''
-    form = forms.AddElectionForm()
-    if form.validate_on_submit():
-        try:
-            election = nuancierlib.add_election(
-                SESSION,
-                election_name=form.election_name.data,
-                election_folder=form.election_folder.data,
-                election_year=form.election_year.data,
-                election_date_start=form.election_date_start.data,
-                election_date_end=form.election_date_end.data,
-                election_n_choice=form.election_n_choice.data,
-                election_badge_link=form.election_badge_link.data,
-            )
-        except nuancierlib.NuancierException as err:
-            flask.flash(err.message, 'error')
-        try:
-            SESSION.commit()
-        except SQLAlchemyError as err:
-            SESSION.rollback()
-            print >> sys.stderr, "Cannot create new election", err
-            flask.flash(err.message, 'error')
-        if form.generate_cache.data:
-            return admin_cache(election.id)
-        return flask.redirect(flask.url_for('admin_index'))
-    return flask.render_template('admin_new.html', form=form)
-
-
 @APP.route('/admin/open/<int:election_id>')
 @nuancier_admin_required
 def admin_open(election_id):
@@ -490,44 +459,6 @@ def admin_open(election_id):
             topic = "open.toggle.on"
         else:
             topic = "open.toggle.off"
-
-        notifications.publish(
-            topic=topic,
-            msg=dict(
-                agent=flask.g.fas_user.username,
-                election=election.api_repr(version=1),
-                state=state,
-            )
-        )
-
-    return flask.redirect(flask.url_for('.admin_index'))
-
-
-@APP.route('/admin/publish/<int:election_id>')
-@nuancier_admin_required
-def admin_publish(election_id):
-    ''' Flip the public state '''
-    election = nuancierlib.get_election(SESSION, election_id)
-    state = nuancierlib.toggle_public(SESSION, election_id)
-
-    if state:
-        msg = "Election published"
-    else:
-        msg = "Election closed"
-
-    try:
-        SESSION.commit()
-    except SQLAlchemyError as err:
-        SESSION.rollback()
-        print >> sys.stderr, "Cannot flip the publish state", err
-        flask.flash(err.message, 'error')
-    else:
-        flask.flash(msg)
-
-        if state:
-            topic = "publish.toggle.on"
-        else:
-            topic = "publish.toggle.off"
 
         notifications.publish(
             topic=topic,
