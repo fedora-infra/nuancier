ace14b8978adf5b5c3ebf8ccae730106697715c3
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-15 11:18:40 +0100

    Remove un-used imports and logging

--- nuancier/lib/model.py
+++ nuancier/lib/model.py
@@ -31,21 +31,12 @@ import logging
 
 import sqlalchemy as sa
 from sqlalchemy import create_engine
-from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import relation
-from sqlalchemy.orm import backref
-from sqlalchemy.orm.collections import attribute_mapped_collection
-from sqlalchemy.orm.collections import mapped_collection
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.sql import and_
-from sqlalchemy.sql.expression import Executable, ClauseElement
 
 BASE = declarative_base()
-
-error_log = logging.getLogger('nuancier.lib.model')
 
 
 def create_tables(db_url, alembic_ini=None, debug=False):
