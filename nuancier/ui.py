3927d5e8e84ba43e1b8ca2a60eb1f4820a5f54d8
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-01-29 14:49:42 +0100

    The submitter should be an mixt of username and email
    This covers the case where the user just created his/her FAS account to submit the
    candidate and thus is not CLA+1 which mean the alias <user>@fp.o is not functional.

--- nuancier/ui.py
+++ nuancier/ui.py
@@ -115,7 +115,8 @@ def contribute(election_id):
                 candidate_author=form.candidate_author.data,
                 candidate_original_url=form.candidate_original_url.data,
                 candidate_license=form.candidate_license.data,
-                candidate_submitter=flask.g.fas_user.username,
+                candidate_submitter='%s -- %s' % (
+                    flask.g.fas_user.username, flask.g.fas_user.email),
                 election_id=election.id
             )
         except nuancierlib.NuancierException as err:
