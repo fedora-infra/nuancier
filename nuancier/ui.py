d2d9527ce77b05b13437157265d8f989d415dd39
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-20 19:10:54 +0100

    Now that we require FAS account to contribute, no need to track the email in the submitter field

--- nuancier/ui.py
+++ nuancier/ui.py
@@ -129,8 +129,7 @@ def contribute(election_id):
                 candidate_author=form.candidate_author.data,
                 candidate_original_url=form.candidate_original_url.data,
                 candidate_license=form.candidate_license.data,
-                candidate_submitter='%s -- %s' % (
-                    flask.g.fas_user.username, flask.g.fas_user.email),
+                candidate_submitter=flask.g.fas_user.username,
                 election_id=election.id,
                 user=flask.g.fas_user.username,
             )
