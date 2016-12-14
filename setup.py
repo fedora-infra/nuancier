643e764d06bbe77e5e73507a0858dcc8d9383f32
Jeremy Cline <jeremy@jcline.org>
2016-12-14 09:37:24 -0500

    Add travis support and fix broken tests
    Depending on how the tests were run, the test for the pictures endpoint
    would fail because the path to the pictures folder would be incorrectly
    calculated since it could be relative. This ensures the path is always
    absolute.
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ Setup script
 """
 
 # Required to build on EL6
-__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
-import pkg_resources
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']  # NOQA
+import pkg_resources  # NOQA
 
+from setuptools import setup
 import os
+import re
 import shutil
 
 
@@ -18,15 +20,39 @@ if os.path.exists('nuancier/static/cache'):
     shutil.rmtree('nuancier/static/cache')
 
 
-from setuptools import setup
-from nuancier import __version__
+def get_version():
+    """Get the current version of the hotness package"""
+    with open('nuancier/__init__.py', 'r') as fd:
+        regex = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+        version = re.search(regex, fd.read(), re.MULTILINE).group(1)
+    if not version:
+        raise RuntimeError('No version set in hotness/__init__.py')
+    return version
+
+
+def get_requirements(requirements_file='requirements.txt'):
+    """Get the contents of a file listing the requirements.
+
+    :arg requirements_file: path to a requirements file
+    :type requirements_file: string
+    :returns: the list of requirements, or an empty list if
+              `requirements_file` could not be opened or read
+    :return type: list
+    """
+
+    lines = open(requirements_file).readlines()
+    return [
+        line.rstrip().split('#')[0]
+        for line in lines
+        if not line.startswith('#')
+    ]
 
 
 setup(
     name='nuancier',
     description='A voting application for the supplementary wallpapers of '
                 'Fedora',
-    version=__version__,
+    version=get_version(),
     author='Pierre-Yves Chibon',
     author_email='pingou@pingoured.fr',
     maintainer='Pierre-Yves Chibon',
@@ -36,6 +62,6 @@ setup(
     url='https://github.com/fedora-infra/nuancier/',
     packages=['nuancier'],
     include_package_data=True,
-    install_requires=['Flask', 'SQLAlchemy>=0.6', 'wtforms', 'flask-wtf',
-                      'python-fedora', 'Pillow', 'dogpile.cache', 'blinker'],
+    install_requires=get_requirements(),
+    tests_require=get_requirements('test-requirements.txt'),
 )
