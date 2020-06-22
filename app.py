93ef9f2898a5d821a65049d324f21cbf5481fe8a
jontrossbach <jtrossba@redhat.com>
2020-06-22 15:30:01 -0400

    minor tweak to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 
+os.system('pip install --upgrade pip')
 os.system('pip3 install -r requirements.txt')
 os.system('python3 createdb.py')
 os.system('sudo firewall-cmd --add-port 5000/tcp --permanent')
