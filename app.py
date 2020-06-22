eef3103962b2179d5a49fd5c085579df28ed6f18
jontrossbach <jtrossba@redhat.com>
2020-06-22 15:14:25 -0400

    add app.py for an s2i entrypoint

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import os
+
+os.system('pip3 install -r requirements.txt')
+os.system('python3 createdb.py')
+os.system('sudo firewall-cmd --add-port 5000/tcp --permanent')
+os.system('sudo firewall-cmd --reload')
+os.system('python3 runserver.py --host 0.0.0.0')
