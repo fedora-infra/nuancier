39b9bd98032eda89d1100f433eff45042ee64734
jontrossbach <jtrossba@redhat.com>
2020-06-22 16:08:00 -0400

    minor tweak to app.py 2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 
 os.system('pip install --upgrade pip')
 os.system('pip3 install -r requirements.txt')
+os.system('cd nuancier_schema && mv ./nuancier_schema/* ./')
 os.system('python3 createdb.py')
 os.system('sudo firewall-cmd --add-port 5000/tcp --permanent')
 os.system('sudo firewall-cmd --reload')
