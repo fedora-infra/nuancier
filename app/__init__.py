88f374553b491d1fc7603c10b2f89520666d64c9
jontrossbach <jtrossba@redhat.com>
2020-06-23 16:46:08 -0400

    fix app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@
 from os import system
 
 if __name__ == "__main__":
-    os.system('python3 ../createdb.py')
-    os.system('python3 ../runserver.py --host 0.0.0.0')
+    system('python3 ../createdb.py')
+    system('python3 ../runserver.py --host 0.0.0.0 --port 8080')
