f1253f648b4d591cef73029cd3627ae3ea8b7380
jontrossbach <jtrossba@redhat.com>
2020-06-23 15:51:58 -0400

    app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from os import system
 
 if __name__ == "__main__":
     system('python3 createdb.py')
-    system('python3 runserver.py --host 0.0.0.0')
+    system('python3 runserver.py --host 0.0.0.0 --port 8080')
 
