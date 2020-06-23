f667571a352df85f1736c04d6f0050a16e693cfb
jontrossbach <jtrossba@redhat.com>
2020-06-23 15:16:40 -0400

    minor fixes for s2i

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@
 from os import system
 
 if __name__ == "__main__":
-    os.system('python3 createdb.py')
-    os.system('python3 runserver.py --host 0.0.0.0')
+    os.system('python3 ../createdb.py')
+    os.system('python3 ../runserver.py --host 0.0.0.0')
