97ddadad970e3b37d65b98eed65ab397bd581bfd
jontrossbach <jtrossba@redhat.com>
2020-07-01 21:12:32 -0400

    Add the OIDC_OPENID_REALM on port 5005

--- nuancier/default_config.py
+++ nuancier/default_config.py
@@ -94,3 +94,8 @@ NUANCIER_EMAIL_ERROR_TO = 'pingou@pingoured.fr'
 
 FEDMENU_URL = 'https://apps.fedoraproject.org/fedmenu'
 FEDMENU_DATA_URL = 'https://apps.fedoraproject.org/js/data.js'
+
+OIDC_CLIENT_SECRETS = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'client_secrets.json')
+OIDC_SCOPES = ['openid', 'email', 'profile', 'fedora']
+OIDC_OPENID_REALM = 'http://localhost:5005/oidc_callback'
