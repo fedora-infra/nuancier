0147dc85249517b5ec29f7f21215f260cced7b0e
jontrossbach <jtrossba@redhat.com>
2020-07-14 14:57:09 -0400

    add config file and call

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     system('python3 createdb.py')
     system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:5005/oidc_callback')
     system('cp client_secrets.json nuancier/client_secrets.json')
-    system('python3 runserver.py --host 0.0.0.0 --port 8080')
+    system('python3 runserver.py --host 0.0.0.0 --port 8080 -c config')
 
