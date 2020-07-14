388c92c17bcda8038824a801086eb20a64b751ed
jontrossbach <jtrossba@redhat.com>
2020-07-14 15:19:13 -0400

    edit oidc-register call to reflect port 8080

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from os import system
 
 if __name__ == "__main__":
     system('python3 createdb.py')
-    system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:5005/oidc_callback')
+    system('oidc-register https://iddev.fedorainfracloud.org/ http://localhost:8080/oidc_callback')
     system('cp client_secrets.json nuancier/client_secrets.json')
     system('python3 runserver.py --host 0.0.0.0 --port 8080 -c config')
 
